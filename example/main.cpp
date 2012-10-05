d76aacf9bb123cef6c7f741625062eac0f9b02bf
rfilippone <rfilippone@gmail.com>
2012-10-05 12:44:46 +0200

    Scoping for third approach is complete

--- example/main.cpp
+++ example/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <map>
+
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/type_traits/is_base_of.hpp>
 #include <boost/mpl/if.hpp>
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
+#include <boost/typeof/typeof.hpp>
+#include <boost/any.hpp>
 
 #include "configurable_factory.h"
 #include "SUT.h"
@@ -119,51 +123,173 @@ private:
 }
 }
 
-struct ScopeType
+
+template <typename T, typename ANNOTATION, typename R> static boost::shared_ptr<T> builder()
 {
-	typedef ScopeType type;
-	template <typename T, typename ANNOTATION, typename R> static boost::shared_ptr<T> get();
-	template <typename T, typename ANNOTATION, typename R> static boost::shared_ptr<T> get(R value);
+    boost::shared_ptr<T> res(boost::make_shared<R>());
+    std::cout << "[Injector] \"" << ANNOTATION::name() << "\" " << typeid(T).name() << " -> " << typeid(R).name() << " = " << res.get() << std::endl;
+    return res;
+}
+
+template <typename T, typename ANNOTATION, typename R> static boost::shared_ptr<T> builder(R value)
+{
+  boost::shared_ptr<T> res(boost::make_shared<R>(value));
+  std::cout << "[Injector] \"" << ANNOTATION::name() << "\" " << typeid(T).name() << " = " << value << std::endl;
+  return res;
+}
+
+//class NoScope
+//{
+//};
+//
+//class LoggerScope
+//{
+//};
+//
+
+struct is_a_scope
+{
+    virtual long getIdx() = 0;
+
+protected:
+    static long next()
+    {
+        static long counter = 0;
+        return (++counter);
+    }
+    is_a_scope() {}
+    virtual ~is_a_scope() {}
+    is_a_scope( const is_a_scope& );
+    const is_a_scope& operator=( const is_a_scope& );
 };
 
-
-struct NoScope : public ScopeType
+struct NOSCOPE : public is_a_scope
 {
-	typedef NoScope type;
-	template <typename T, typename ANNOTATION, typename R> static boost::shared_ptr<T> get()
-	{
-		boost::shared_ptr<T> res(boost::make_shared<R>());
-		std::cout << "[Injector] {" << typeid(type).name() << "} \"" << ANNOTATION::name() << "\" " << typeid(T).name() << " -> " << typeid(R).name() << " = " << res.get() << std::endl;
-		return res;
-	}
+    template<typename T> static boost::function<boost::shared_ptr<T>()> scope(boost::function<boost::shared_ptr<T>()> unscopedProvider)
+    {
+        std::cout << "NOSCOPE::scope called" << std::endl;
+        return unscopedProvider;
+    }
 
-	template <typename T, typename ANNOTATION, typename R> static boost::shared_ptr<T> get(R value)
-	{
-	    boost::shared_ptr<T> res(boost::make_shared<R>(value));
-	    std::cout << "[Injector] {" << typeid(type).name() << "} \"" << ANNOTATION::name() << "\" " << typeid(T).name() << " = " << value << std::endl;
-	    return res;
-	}
+    static NOSCOPE* instance()
+    {
+        if (!m_instance)
+        {
+            m_instance = new NOSCOPE();
+        }
+        return m_instance;
+    }
+
+    static std::string name()
+    {
+        return "NOSCOPE";
+    }
+
+    static long const idx()
+    {
+        static long counter = is_a_scope::next();
+        return counter;
+    }
+
+    long getIdx()
+    {
+        return idx();
+    }
+private:
+    static NOSCOPE* m_instance;
+    NOSCOPE() {}
+    ~NOSCOPE() {};
+    NOSCOPE( const NOSCOPE& );
+    const NOSCOPE& operator=( const NOSCOPE& );
 };
+NOSCOPE* NOSCOPE::m_instance = NULL;
+
+struct LoggerScope : public is_a_scope
+{
+    template<typename T> static boost::function<boost::shared_ptr<T>()> scope(boost::function<boost::shared_ptr<T>()> unscopedProvider)
+    {
+        std::cout << "LoggerScope::scope called" << std::endl;
+
+        return boost::bind(LoggerScope::scopedProvider<T>, unscopedProvider);
+    }
+
+    template<typename T> static boost::shared_ptr<T> scopedProvider(boost::function<boost::shared_ptr<T>()> unscopedProvider)
+    {
+        std::cout << "LoggerScope scoping called" << std::endl;
+        boost::shared_ptr<T> res = unscopedProvider();
+        std::cout << "LoggerScope scoping finished" << std::endl;
+        return res;
+    }
+
+    static LoggerScope* instance()
+    {
+        if (!m_instance)
+        {
+            m_instance = new LoggerScope();
+        }
+        return m_instance;
+    }
+
+    static std::string name()
+    {
+        return "LoggerScope";
+    }
+
+    static long const idx()
+    {
+        static long counter = is_a_scope::next();
+        return counter;
+    }
+
+    long getIdx()
+    {
+        return idx();
+    }
+private:
+    static LoggerScope* m_instance;
+    LoggerScope() {}
+    ~LoggerScope() {};
+    LoggerScope( const NOSCOPE& );
+    const LoggerScope& operator=( const NOSCOPE& );
+};
+LoggerScope* LoggerScope::m_instance = NULL;
 
 
 template <typename T, typename ANNOTATION=factory::annotation::FREE> class Injector;
+template <typename T, typename ANNOTATION, typename SCOPE> class ScopedBindingStore;
 
 template <typename T> class bind
 {
 public:
     template <typename R> static void to()
     {
-        Injector<T>::bprovider = boost::bind(NoScope::get<T, factory::annotation::FREE, R>);
+        Injector<T>::unscopedProvider = boost::bind(builder<T, factory::annotation::FREE, R>);
     }
 
     template <typename R> static void to(R value)
     {
-        Injector<T>::bprovider = boost::bind(NoScope::get<T, factory::annotation::FREE, R>, value);
+        Injector<T>::unscopedProvider = boost::bind(builder<T, factory::annotation::FREE, R>, value);
     }
 
     template <typename A> class annotatedWith
     {
     public:
+
+        template <typename S> class in
+        {
+        public:
+
+            template <typename R> static void to()
+            {
+                annotatedWith_in_to<A, S, R>();
+            }
+
+            template <typename R> static void to(R value)
+            {
+                annotatedWith_in_to<A, S, R>(value);
+            }
+        };
+
         template <typename R> static void to()
         {
             annotatedWith_to<A, R>();
@@ -176,24 +302,103 @@ public:
     };
 
 
-    template <typename A> class in
+    template <typename S> class in
     {
     public:
 
+        template <typename A> class annotatedWith
+        {
+        public:
+            template <typename R> static void to()
+            {
+                annotatedWith_in_to<A, S, R>();
+            }
 
+            template <typename R> static void to(R value)
+            {
+                annotatedWith_in_to<A, S, R>(value);
+            }
+        };
+
+        template <typename R> static void to()
+        {
+            annotatedWith_in_to<factory::annotation::FREE, S, R>();
+        }
+
+        template <typename R> static void to(R value)
+        {
+            annotatedWith_in_to<factory::annotation::FREE, S, R>(value);
+        }
     };
 
 private:
     template <typename A, typename R> static void annotatedWith_to()
     {
-        Injector<T, A>::bprovider = boost::bind(NoScope::get<T, A, R>);
+        Injector<T, A>::unscopedProvider = boost::bind(builder<T, A, R>);
     }
 
     template <typename A, typename R> static void annotatedWith_to(R value)
     {
-        Injector<T, A>::bprovider = boost::bind(NoScope::get<T, A, R>, value);
+        Injector<T, A>::unscopedProvider = boost::bind(builder<T, A, R>, value);
     }
+
+//    template <typename S, typename R> static void in_to()
+//    {
+//        ScopedBindingStore<T, factory::annotation::FREE, S>::provider = LoggerScope::scope<T>(boost::bind<boost::shared_ptr<T> >(builder<T, factory::annotation::FREE, R>));
+//        ScopedBindingStore<T, factory::annotation::FREE, S>::set();
+//    }
+//
+//    template <typename S, typename R> static void in_to(R value)
+//    {
+//        ScopedBindingStore<T, factory::annotation::FREE, S>::provider = LoggerScope::scope<T>(boost::bind<boost::shared_ptr<T> >(builder<T, factory::annotation::FREE, R>, value));
+//        ScopedBindingStore<T, factory::annotation::FREE, S>::set();
+//    }
+
+    template <typename A, typename S, typename R> static void annotatedWith_in_to()
+    {
+        ScopedBindingStore<T, A, S>::provider = LoggerScope::scope<T>(boost::bind<boost::shared_ptr<T> >(builder<T, A, R>));
+        ScopedBindingStore<T, A, S>::set();
+    }
+
+    template <typename A, typename S, typename R> static void annotatedWith_in_to(R value)
+    {
+        ScopedBindingStore<T, A, S>::provider = LoggerScope::scope<T>(boost::bind<boost::shared_ptr<T> >(builder<T, A, R>, value));
+        ScopedBindingStore<T, A, S>::set();
+    }
+
 };
+
+
+
+template <typename T, typename ANNOTATION, typename SCOPE> class ScopedBindingStore
+{
+public:
+
+    static void set()
+    {
+        Injector<T,ANNOTATION>::providerMap.insert(std::make_pair(SCOPE::idx(),provider));
+    }
+
+    static boost::function<boost::shared_ptr<T>()> provider;
+};
+template<typename T, typename ANNOTATION, typename SCOPE> boost::function<boost::shared_ptr<T>()> ScopedBindingStore<T, ANNOTATION, SCOPE>::provider = NULL;
+
+
+struct Scope
+{
+    template<typename S> static void enter()
+    {
+        currentScope = S::instance();
+    }
+
+    static void exit()
+    {
+        currentScope = NULL;
+    }
+    static is_a_scope* currentScope;
+};
+is_a_scope* Scope::currentScope = NULL;
+
 
 template <typename T, typename ANNOTATION> class Injector
 {
@@ -201,22 +406,27 @@ template <typename T, typename ANNOTATION> class Injector
 public:
     static boost::shared_ptr<T> get()
     {
-        return bprovider();
+        if (Scope::currentScope)
+        {
+            std::cout << "get " << Scope::currentScope->getIdx() << std::endl;
+
+            typename std::map<long, boost::function<boost::shared_ptr<T>()> >::iterator iter = providerMap.find(Scope::currentScope->getIdx());
+            if (iter != providerMap.end())
+            {
+                return iter->second();
+            }
+        }
+        return unscopedProvider();
     }
 
+    static std::map<long, boost::function<boost::shared_ptr<T>()> > providerMap;
 private:
-    static boost::function<boost::shared_ptr<T>()> bprovider;
-//    static std::map<ScopeType, int >& getScopeMap()
-//    {
-//        static std::map<ScopeType, int > scopeMap;
-//        return scopeMap;
-//    }
+    static boost::function<boost::shared_ptr<T>()> unscopedProvider;
 
     friend class bind<T>;
 };
-template<typename T, typename ANNOTATION> boost::function<boost::shared_ptr<T>()> Injector<T, ANNOTATION>::bprovider = boost::bind(NoScope::get<T, ANNOTATION, T>);
-
-
+template<typename T, typename ANNOTATION> boost::function<boost::shared_ptr<T>()> Injector<T, ANNOTATION>::unscopedProvider = boost::bind(builder<T, ANNOTATION, T>);
+template<typename T, typename ANNOTATION> std::map<long, boost::function<boost::shared_ptr<T>()> > Injector<T, ANNOTATION>::providerMap;
 
 class ParamAlternativeDatabase : public DatabaseInterface
 {
@@ -255,64 +465,64 @@ template<typename T> inline T* build()
 
 int main()
 {
-    std::cout << "################# 1st approach" << std::endl;
-
-    SUT realInstance;
-    realInstance.doSomething();
-
-    std::cout << "-----------------" << std::endl;
-
-    Factory<DatabaseInterface>::replace<AlternativeDatabase>();
-    SUT alternativeInstance;
-    alternativeInstance.doSomething();
-    Factory<DatabaseInterface>::reset();
-
-
-    std::cout << "-----------------" << std::endl;
-
-    Factory<DatabaseInterface>::replace<ParamAlternativeDatabase, std::string>();
-    SUT parameterInstance;
-    parameterInstance.doSomething();
-    Factory<DatabaseInterface>::reset<std::string>();
-    Factory<DatabaseInterface>::reset();
-
-    std::cout << "-----------------" << std::endl;
-
-    Factory<DatabaseInterface>::replace(my_factory);
-    SUT functionInstance;
-    functionInstance.doSomething();
-    Factory<DatabaseInterface>::reset();
-
-    std::cout << "-----------------" << std::endl;
-
-    MockDatabase* mock = new MockDatabase(1);
-    Factory<DatabaseInterface>::inject(mock);
-
-    MockDatabase* mock2 = new MockDatabase(2);
-    Factory<DatabaseInterface, factory::scopes::MYSCOPE>::inject(mock2);
-
-    //set expectation on mock here
-    SUT testInstance;
-    testInstance.doSomething();
-    Factory<DatabaseInterface>::reset();
-
-    std::cout << "################# 2nd approach" << std::endl;
-
-    boost::shared_ptr<DatabaseInterface> p1,p2,p3,p4,p5,p6,p7,p8,p9;
-
-    p1 = CF<DatabaseInterface>::get();
-
-    p2 = CF<DatabaseInterface, factory::scopes::MYSCOPE2>::get();
-    p3 = CF<DatabaseInterface, void, factory::scopes::MYSCOPE2>::get();
-    p4 = CF<DatabaseInterface, factory::scopes::MYSCOPE2, void >::get();
-
-    p5 = CF<DatabaseInterface, factory::names::NEWNAME>::get();
-    p6 = CF<DatabaseInterface, void, factory::names::NEWNAME>::get();
-    p7 = CF<DatabaseInterface, factory::names::NEWNAME, void >::get();
-
-    p8 = CF<DatabaseInterface, factory::scopes::MYSCOPE2, factory::names::NEWNAME>::get();
-    p9 = CF<DatabaseInterface, factory::names::NEWNAME, factory::scopes::MYSCOPE2>::get();
-
+//    std::cout << "################# 1st approach" << std::endl;
+//
+//    SUT realInstance;
+//    realInstance.doSomething();
+//
+//    std::cout << "-----------------" << std::endl;
+//
+//    Factory<DatabaseInterface>::replace<AlternativeDatabase>();
+//    SUT alternativeInstance;
+//    alternativeInstance.doSomething();
+//    Factory<DatabaseInterface>::reset();
+//
+//
+//    std::cout << "-----------------" << std::endl;
+//
+//    Factory<DatabaseInterface>::replace<ParamAlternativeDatabase, std::string>();
+//    SUT parameterInstance;
+//    parameterInstance.doSomething();
+//    Factory<DatabaseInterface>::reset<std::string>();
+//    Factory<DatabaseInterface>::reset();
+//
+//    std::cout << "-----------------" << std::endl;
+//
+//    //Factory<DatabaseInterface>::replace(my_factory);
+//    SUT functionInstance;
+//    functionInstance.doSomething();
+//    Factory<DatabaseInterface>::reset();
+//
+//    std::cout << "-----------------" << std::endl;
+//
+//    MockDatabase* mock = new MockDatabase(1);
+//    Factory<DatabaseInterface>::inject(mock);
+//
+//    MockDatabase* mock2 = new MockDatabase(2);
+//    Factory<DatabaseInterface, factory::scopes::MYSCOPE>::inject(mock2);
+//
+//    //set expectation on mock here
+//    SUT testInstance;
+//    testInstance.doSomething();
+//    Factory<DatabaseInterface>::reset();
+//
+//    std::cout << "################# 2nd approach" << std::endl;
+//
+//    boost::shared_ptr<DatabaseInterface> p1,p2,p3,p4,p5,p6,p7,p8,p9;
+//
+//    p1 = CF<DatabaseInterface>::get();
+//
+//    p2 = CF<DatabaseInterface, factory::scopes::MYSCOPE2>::get();
+//    p3 = CF<DatabaseInterface, void, factory::scopes::MYSCOPE2>::get();
+//    p4 = CF<DatabaseInterface, factory::scopes::MYSCOPE2, void >::get();
+//
+//    p5 = CF<DatabaseInterface, factory::names::NEWNAME>::get();
+//    p6 = CF<DatabaseInterface, void, factory::names::NEWNAME>::get();
+//    p7 = CF<DatabaseInterface, factory::names::NEWNAME, void >::get();
+//
+//    p8 = CF<DatabaseInterface, factory::scopes::MYSCOPE2, factory::names::NEWNAME>::get();
+//    p9 = CF<DatabaseInterface, factory::names::NEWNAME, factory::scopes::MYSCOPE2>::get();
+//
     std::cout << "################# 3rd approach" << std::endl;
 
     boost::shared_ptr<DatabaseInterface> i1,i2,i3,i4,i5,i6,i7,i8,i9;
@@ -321,6 +531,10 @@ int main()
     bind<DatabaseInterface>::to<AlternativeDatabase>();
     bind<int>::to(145);
     bind<int>::annotatedWith<factory::annotation::NEWANNOTATION>::to(157);
+
+    //bind<int>::in<LoggerScope>::to<int>(74);
+    bind<int>::to(74);
+    bind<int>::in<LoggerScope>::annotatedWith<factory::annotation::NEWANNOTATION>::to<int>(1234);
 
     i2 = Injector<DatabaseInterface, factory::annotation::NEWANNOTATION>::get();
     i1 = Injector<DatabaseInterface>::get();
@@ -331,6 +545,13 @@ int main()
 
     boost::shared_ptr<int> n2 = Injector<int, factory::annotation::NEWANNOTATION>::get();
     std::cout << "n2 = " << *n2 << std::endl;
+
+    Scope::enter<NOSCOPE>();
+
+    Scope::enter<LoggerScope>();
+
+    boost::shared_ptr<int> n3 = Injector<int, factory::annotation::NEWANNOTATION>::get();
+    std::cout << "n3 = " << *n3 << std::endl;
 
     return 0;
 }
